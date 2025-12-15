630c0c35a45092336c808f1f11ec04ab3ac49432
Nayan Mondal <n007kingkong@gmail.com>
2025-12-15 16:00:13 +0600

    some changes is implemented

--- src/Pages/AllModels.jsx
+++ src/Pages/AllModels.jsx
@@ -88,7 +88,7 @@ const AllModels = () => {
             </div>
 
             {/* Models Grid */}
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 px-6 md:px-14'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-4 px-6 md:px-14 mb-8'>
                 {
                     models?.map(model => <Card key={model._id} model={model} />)
                 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
         <div className='px-4 md:px-14'>
             <Slider></Slider>
             <h1 className='text-2xl font-bold my-4'>Latest Models</h1>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3   gap-4'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-8   gap-4'>
             {
                 models.map(model => <Card model={model}></Card>)
             }
